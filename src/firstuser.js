50b3123f2943b5911cd423afeb6edf13fe176f4a
Arvind Arcgate <arvind.deora@arcgate.com>
2025-09-11 14:17:26 +0530

    Conflict resolve

--- src/firstuser.js
+++ src/firstuser.js
@@ -1,4 +1,2 @@
 
-console.log("two people commit same time I am first user ");
-
-console.log("First person have write the code line . I want to implement my line");
+console.log("two people commit same time I am first user and we both will merge together ");
